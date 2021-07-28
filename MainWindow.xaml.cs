7e569c7bcf97c24e73f7061a729210d4b5435928
HiP3X <bgsamer3@gmail.com>
2021-07-28 18:32:24 +0300

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace d3EtherealTracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Doombringer_Click(object sender, RoutedEventArgs e)
+        {
+            if(Doombringer.Foreground == Brushes.LimeGreen)
+            {
+                Doombringer.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Doombringer.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void Gimmershred_Click(object sender, RoutedEventArgs e)
+        {
+            if(Gimmershred.Foreground == Brushes.LimeGreen)
+            {
+                Gimmershred.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Gimmershred.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void The_Grandfather_Click(object sender, RoutedEventArgs e)
+        {
+            if(The_Grandfather.Foreground == Brushes.LimeGreen)
+            {
+                The_Grandfather.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                The_Grandfather.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void Astreon_s_Iron_Ward_Click(object sender, RoutedEventArgs e)
+        {
+            if(Astreon_s_Iron_Ward.Foreground == Brushes.LimeGreen)
+            {
+                Astreon_s_Iron_Ward.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Astreon_s_Iron_Ward.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void Khalim_s_Will_Click(object sender, RoutedEventArgs e)
+        {
+            if(Khalim_s_Will.Foreground == Brushes.LimeGreen)
+            {
+                Khalim_s_Will.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Khalim_s_Will.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void The_Redeemer_Click(object sender, RoutedEventArgs e)
+        {
+            if(The_Redeemer.Foreground == Brushes.LimeGreen)
+            {
+                The_Redeemer.Foreground = Brushes.Teal;
+            }    
+            else
+            {
+                The_Redeemer.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void Buriza_Do_Kyanon_Click(object sender, RoutedEventArgs e)
+        {
+            if(Buriza_Do_Kyanon.Foreground == Brushes.LimeGreen)
+            {
+                Buriza_Do_Kyanon.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Buriza_Do_Kyanon.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void Doomslinger_Click(object sender, RoutedEventArgs e)
+        {
+            if(Doomslinger.Foreground == Brushes.LimeGreen)
+            {
+                Doomslinger.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Doomslinger.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void Windforce_Click(object sender, RoutedEventArgs e)
+        {
+            if(Windforce.Foreground == Brushes.LimeGreen)
+            {
+                Windforce.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Windforce.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void Bartuc_s_Cut_Throat_Click(object sender, RoutedEventArgs e)
+        {
+            if(Bartuc_s_Cut_Throat.Foreground == Brushes.LimeGreen)
+            {
+                Bartuc_s_Cut_Throat.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Bartuc_s_Cut_Throat.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void Jade_Talon_Click(object sender, RoutedEventArgs e)
+        {
+            if(Jade_Talon.Foreground == Brushes.LimeGreen)
+            {
+                Jade_Talon.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Jade_Talon.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void Shadow_Killer_Click(object sender, RoutedEventArgs e)
+        {
+            if(Shadow_Killer.Foreground == Brushes.LimeGreen)
+            {
+                Shadow_Killer.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Shadow_Killer.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void Blackbog_s_Sharp_Click(object sender, RoutedEventArgs e)
+        {
+            if(Blackbog_s_Sharp.Foreground == Brushes.LimeGreen)
+            {
+                Blackbog_s_Sharp.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Blackbog_s_Sharp.Foreground = Brushes.LimeGreen;
+            }
+        }
+        private void Blackhand_Key_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(Blackhand_Key.Foreground == Brushes.LimeGreen)
+            {
+                Blackhand_Key.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Blackhand_Key.Foreground = Brushes.LimeGreen; 
+            }
+        }
+
+        private void Soul_Harvest_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(Soul_Harvest.Foreground == Brushes.LimeGreen)
+            {
+                Soul_Harvest.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Soul_Harvest.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void Arioc_s_Needle_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(Arioc_s_Needle.Foreground == Brushes.LimeGreen)
+            {
+                Arioc_s_Needle.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Arioc_s_Needle.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void Ghostflame_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(Ghostflame.Foreground == Brushes.LimeGreen)
+            {
+                Ghostflame.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Ghostflame.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void The_Gidbinn_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(The_Gidbinn.Foreground == Brushes.LimeGreen)
+            {
+                The_Gidbinn.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                The_Gidbinn.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void Mang_Song_s_Lesson_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(Mang_Song_s_Lesson.Foreground == Brushes.LimeGreen)
+            {
+                Mang_Song_s_Lesson.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Mang_Song_s_Lesson.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void The_Oculus_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(The_Oculus1.Foreground == Brushes.LimeGreen)
+            {
+                The_Oculus1.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                The_Oculus1.Foreground = Brushes.LimeGreen;
+            }
+        }
+
+        private void Wizardspike_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(Wizardspike.Foreground == Brushes.LimeGreen)
+            {
+                Wizardspike.Foreground = Brushes.Teal;
+            }
+            else
+            {
+                Wizardspike.Foreground = Brushes.LimeGreen;
+            }
+        }
+    }
+}
